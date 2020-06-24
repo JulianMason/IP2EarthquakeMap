83fe5b969d46acabb7bf3f036098fde436f1ac0e
JulianMason <JulianMason@users.noreply.github.com>
2020-06-24 04:38:20 +0100

    Map fixed

--- sketch2.js
+++ sketch2.js
@@ -1,14 +1,4 @@
-/**
- * ---------------------------------------
- * This demo was created using amCharts 4.
- * 
- * For more information visit:
- * https://www.amcharts.com/
- * 
- * Documentation is available at:
- * https://www.amcharts.com/docs/v4/
- * ---------------------------------------
- */
+
 
 // Themes begin
 am4core.useTheme(am4themes_animated);
@@ -42,10 +32,15 @@ polygonTemplate.polygon.fillOpacity = 0.6;
 var hs = polygonTemplate.states.create("hover");
 hs.properties.fill = chart.colors.getIndex(0);
 
+//Load external data
+//chart.dataSource.url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson";
+
+var dataSource = chart.dataSource.url;
+
 // Add image series
 var imageSeries = chart.series.push(new am4maps.MapImageSeries());
-imageSeries.mapImages.template.propertyFields.longitude = "longitude";
-imageSeries.mapImages.template.propertyFields.latitude = "latitude";
+imageSeries.mapImages.template.propertyFields.longitude = "latitude";
+imageSeries.mapImages.template.propertyFields.latitude = "longitude";
 imageSeries.mapImages.template.tooltipText = "{title}";
 imageSeries.mapImages.template.propertyFields.url = "url";
 
@@ -72,97 +67,26 @@ function animateBullet(circle) {
 
 var colorSet = new am4core.ColorSet();
 
-imageSeries.data = [ {
-  "title": "Brussels",
-  "latitude": 50.8371,
-  "longitude": 4.3676,
-  "color":colorSet.next()
-}, {
-  "title": "Copenhagen",
-  "latitude": 55.6763,
-  "longitude": 12.5681,
-  "color":colorSet.next()
-}, {
-  "title": "Paris",
-  "latitude": 48.8567,
-  "longitude": 2.3510,
-  "color":colorSet.next()
-}, {
-  "title": "Reykjavik",
-  "latitude": 64.1353,
-  "longitude": -21.8952,
-  "color":colorSet.next()
-}, {
-  "title": "Moscow",
-  "latitude": 55.7558,
-  "longitude": 37.6176,
-  "color":colorSet.next()
-}, {
-  "title": "Madrid",
-  "latitude": 40.4167,
-  "longitude": -3.7033,
-  "color":colorSet.next()
-}, {
-  "title": "London",
-  "latitude": 51.5002,
-  "longitude": -0.1262,
-  "url": "http://www.google.co.uk",
-  "color":colorSet.next()
-}, {
-  "title": "Peking",
-  "latitude": 39.9056,
-  "longitude": 116.3958,
-  "color":colorSet.next()
-}, {
-  "title": "New Delhi",
-  "latitude": 28.6353,
-  "longitude": 77.2250,
-  "color":colorSet.next()
-}, {
-  "title": "Tokyo",
-  "latitude": 35.6785,
-  "longitude": 139.6823,
-  "url": "http://www.google.co.jp",
-  "color":colorSet.next()
-}, {
-  "title": "Ankara",
-  "latitude": 39.9439,
-  "longitude": 32.8560,
-  "color":colorSet.next()
-}, {
-  "title": "Buenos Aires",
-  "latitude": -34.6118,
-  "longitude": -58.4173,
-  "color":colorSet.next()
-}, {
-  "title": "Brasilia",
-  "latitude": -15.7801,
-  "longitude": -47.9292,
-  "color":colorSet.next()
-}, {
-  "title": "Ottawa",
-  "latitude": 45.4235,
-  "longitude": -75.6979,
-  "color":colorSet.next()
-}, {
-  "title": "Washington",
-  "latitude": 38.8921,
-  "longitude": -77.0241,
-  "color":colorSet.next()
-}, {
-  "title": "Kinshasa",
-  "latitude": -4.3369,
-  "longitude": 15.3271,
-  "color":colorSet.next()
-}, {
-  "title": "Cairo",
-  "latitude": 30.0571,
-  "longitude": 31.2272,
-  "color":colorSet.next()
-}, {
-  "title": "Pretoria",
-  "latitude": -25.7463,
-  "longitude": 28.1876,
-  "color":colorSet.next()
-} ];
+$.getJSON("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson", function(earthquake) {
+
+    imageSeries.data = [ {
+
+    "title": earthquake.features[8].properties.title,
+    "latitude": earthquake.features[8].geometry.coordinates[0],
+    "longitude": earthquake.features[8].geometry.coordinates[1],
+    "color": colorSet.next()
+    }];
+   
+     /** for (var i = 0; i < earthquake.features.length; i++) {
+      imageSeries.data = [ {
+
+      "title": earthquake.features[i].properties.title,
+      "latitude": earthquake.features[i].geometry.coordinates[0],
+      "longitude": earthquake.features[i].geometry.coordinates[1],
+      "color": colorSet.next()
+      }];
+    }**/
+
+})
+
 
